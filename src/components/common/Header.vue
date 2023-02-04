36dac75dbc703403c1c1e022556e56159a9c2b03
define9 <define9@qq.com>
2023-02-04 12:33:26 +0800

    修改一下导航

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,11 +1,11 @@
 <template>
     <el-row>
-        <el-col :span="3">1</el-col>
+        <el-col :span="3"></el-col>
         <el-col :span="6">
             <Menu></Menu>
         </el-col>
-        <el-col :span="6">2</el-col>
-        <el-col :span="3">3</el-col>
+        <el-col :span="6"></el-col>
+        <el-col :span="3"></el-col>
         <el-col :span="4">
             <el-input @keyup.enter.native="search" placeholder="键入以搜索工具" v-model="searchText">
                 <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
@@ -37,3 +37,9 @@ export default ({
     }
 })
 </script>
+
+<style scoped>
+.el-col {
+    min-height: 1px;
+}
+</style>
